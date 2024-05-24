2852b9773d70019a76895c4df94ee0f02a830f8c
Alvaruto12 <alvaro.marchant@usm.cl>
2024-05-24 17:58:45 -0400

    creacion de forms.py

--- login/app1/views.py
+++ login/app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from .forms import CustomUserCreationForm
 # Create your views here.
 def home(request):
     return render(request, 'app1/home.html')
@@ -12,3 +13,6 @@ def products(request):
 def exit(request):
     logout(request)
     return redirect('home')
+
+def register(request):
+    return render(request, 'registration/register.html')
