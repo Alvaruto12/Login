079cf4b14b3d67496f27565bb1a9e485f5e1832d
Alvaruto12 <alvaro.marchant@usm.cl>
2024-05-25 01:11:16 -0400

    beta1

--- login/app1/views.py
+++ login/app1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def home(request):
     return render(request, 'app1/home.html')
@@ -15,7 +16,15 @@ def exit(request):
     return redirect('home')
 
 def register(request):
-    data ={
+    data = {
         'form': CustomUserCreationForm()
     }
+    if request.method == 'POST':
+        user_creation_form = CustomUserCreationForm(data=request.POST)
+        
+        if user_creation_form.is_valid():
+            user_creation_form.save()
+            user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
+            login(request, user) 
+            return redirect('home')
     return render(request, 'registration/register.html', data)
